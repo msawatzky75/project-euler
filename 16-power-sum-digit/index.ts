db53725e5fdfebaaa6be0d0256573b3335b25fbf
Matthew Sawatzky <34319870+msawatzky75@users.noreply.github.com>
2019-08-16 16:22:52 -0500

    started strint class

--- 16-power-sum-digit/index.ts
+++ 16-power-sum-digit/index.ts
@@ -1,19 +1,4 @@
 // incomplete
-function sumDigits(num: number): number {
-	let sum = 0;
-	if (num.toString().length > 1) {
-		sum += sumDigits(removeFirstNum(num));
-	}
-	return sum + Number(num.toString().charAt(0));
-}
-
-function removeFirstNum(num: number): number {
-	const magnitude = Math.pow(10, Math.floor(Math.log10(num)));
-	return num - (magnitude * Math.floor(num / magnitude))
-}
+import {sumDigits} from '../lib';
 
 console.log(sumDigits(Math.pow(2, 1000)));
-// console.log(sumDigits(Math.pow(2, 15)));
-
-// console.log("sum of the digits", sumDigits(123));
-// console.log(removeFirstNum(123));
--- lib/index.ts
+++ lib/index.ts
@@ -12,3 +12,26 @@ export function Array2D<T>(length: number, defaultValue: T | null = null): T[][]
 		}
 		return array;
 }
+
+function removeFirstNum(num: number): number {
+	const magnitude = Math.pow(10, Math.floor(Math.log10(num)));
+	return num - (magnitude * Math.floor(num / magnitude))
+}
+
+//too inaccurate for long numbers
+export function sumDigits(num: number): number {
+	console.log(num);
+	let sum = 0;
+	if (num.toString().length > 1) {
+		sum += sumDigits(removeFirstNum(num));
+	}
+	return sum + Number(num.toString().charAt(0));
+}
+
+export class StrInt {
+	num: string;
+
+	constructor(num) {
+		this.num = num.toString() || "";
+	}
+}
