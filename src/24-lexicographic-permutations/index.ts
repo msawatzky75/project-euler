b52b9ceec683fa1b28981a6f54abb4f4b4e1cd2c
Matthew Sawatzky <msawatzky75@gmail.com>
2021-12-13 20:30:19 -0600

    github co-pilot for the win

--- src/24-lexicographic-permutations/index.ts
+++ src/24-lexicographic-permutations/index.ts
@@ -1,7 +1,22 @@
-let nums = [];
-for (let i = 1000000000; i < 10000000000; i++) {
-	nums.push(i.toString().padStart(10, "0"));
-	if (nums.length === 1000000 + 1) break;
+// find the 1 000 000th lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9.
+const input = "012";
+const permutations = permute(input);
+console.log(permutations[999_999]);
+
+function permute(input) {
+	const result = [];
+	permuteRecursive(input, result, "");
+	return result;
 }
-nums.sort((a, b) => a - b);
-console.log(nums[1000000]);
+
+function permuteRecursive(input: string, result: string[], current: string) {
+	if (input.length === 0) {
+		console.log(current);
+
+		result.push(current);
+		return;
+	}
+	for (let i = 0; i < input.length; i++) {
+		permuteRecursive(input.slice(0, i) + input.slice(i + 1), result, current + input[i]);
+	}
+}
